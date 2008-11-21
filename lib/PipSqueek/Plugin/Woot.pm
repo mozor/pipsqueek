2e698036cce634d75db3dfed2f1ea65310cf6839
Zach Fouts <superzach@gmail.com>
2008-11-21 03:37:16 +0000

    Updated the Woot module.. Fixed woot-tracker.com to comply with the module. Adds two additional prefixes now. !shirt and !wine as well as !woot. It grabs the info from http://www.woot-tracker.com still.

--- lib/PipSqueek/Plugin/Woot.pm
+++ lib/PipSqueek/Plugin/Woot.pm
@@ -7,25 +7,61 @@ require LWP::UserAgent;
  {
    my $self = shift;
    $self->plugin_handlers({
-                  'multi_woot'      => 'woot_checker'
+                  'multi_woot'      => 'woot_checker',
+                  'multi_shirt'      => 'shirt_checker',
+                  'multi_wine'      => 'wine_checker',
+
 });
 
  }
 sub woot_checker {
    my ($self,$message) = @_;
    my $uaw = LWP::UserAgent->new;
-	$uaw->timeout(15);
-	
+        $uaw->timeout(15);
+
    $uaw->proxy(['http','ftp'], $self->config()->plugin_proxy()) if ($self->config()->plugin_proxy());
-   my $woot = $uaw->get('http://www.woot-tracker.com/pips/index.php');
-	
-	if ($woot->is_success) {
-		return $self->respond( $message,($woot->content) );
-	
-	} else {
-        		return $self->respond( $message,("An error has occurred.") );
+   my $woot = $uaw->get('http://www.woot-tracker.net/pips/index.php');
+
+        if ($woot->is_success) {
+                return $self->respond( $message,($woot->content) );
+
+        } else {
+                        return $self->respond( $message,("An error has occurred.") );
         }
 }
+
+sub wine_checker {
+   my ($self,$message) = @_;
+   my $uaw = LWP::UserAgent->new;
+        $uaw->timeout(15);
+
+   $uaw->proxy(['http','ftp'], $self->config()->plugin_proxy()) if ($self->config()->plugin_proxy());
+   my $woot = $uaw->get('http://www.woot-tracker.net/pips/wine.php');
+
+        if ($woot->is_success) {
+                return $self->respond( $message,($woot->content) );
+
+        } else {
+                        return $self->respond( $message,("An error has occurred.") );
+        }
+}
+
+sub shirt_checker {
+   my ($self,$message) = @_;
+   my $uaw = LWP::UserAgent->new;
+        $uaw->timeout(15);
+
+   $uaw->proxy(['http','ftp'], $self->config()->plugin_proxy()) if ($self->config()->plugin_proxy());
+   my $woot = $uaw->get('http://www.woot-tracker.net/pips/shirt.php');
+
+        if ($woot->is_success) {
+                return $self->respond( $message,($woot->content) );
+
+        } else {
+                        return $self->respond( $message,("An error has occurred.") );
+        }
+}
+
 
 1;
 
