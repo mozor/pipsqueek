5f3901005de3da1f15588455f026ce8d76a7d121
Shaun Guth <shaun.guth@gmail.com>
2010-07-07 22:37:29 +0000

    Calculates BMI

---
+++ lib/PipSqueek/Plugin/BMI.pm
@@ -0,0 +1,45 @@
+package PipSqueek::Plugin::BMI;
+use base qw(PipSqueek::Plugin);
+
+sub plugin_initialize {
+    my $self = shift;
+
+    $self->plugin_handlers(
+        [
+            'multi_bmi',
+        ]
+    );
+}
+
+sub multi_bmi {
+    my ( $self, $message ) = @_;
+    my ( $height, $weight ) = split /\s+/, $message->command_input();
+
+    # bmi = (weight / height^2) * 703
+
+    if ( !$height || !$weight ) {
+        return $self->respond(
+            $message,
+            "Usage: !bmi <height (in)> <weight (lb)>"
+        );
+    }
+
+    my $bmi = ( $weight / ( $height * $height ) ) * 703;
+
+    if ( $bmi < 18.5 ) {
+        return $self->respond( $message, "BMI: $bmi - Underweight" );
+    } elsif ( $bmi < 25 ) {
+        return $self->respond( $message, "BMI: $bmi - Normal" );
+    } elsif ( $bmi < 30 ) {
+        return $self->respond( $message, "BMI: $bmi - Overweight" );
+    } else {
+        return $self->respond( $message, "BMI: $bmi - Fatty" );
+    }
+}
+
+
+1;
+
+
+__END__
+
