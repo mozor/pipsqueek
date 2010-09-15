3b0ada246961329a26b72f871b71e4f5ce386407
Zach Fouts <superzach@gmail.com>
2010-09-15 08:16:56 +0000

    updated Woot.pm for all those who even care...

--- lib/PipSqueek/Plugin/Woot.pm
+++ lib/PipSqueek/Plugin/Woot.pm
@@ -10,7 +10,10 @@ require LWP::UserAgent;
                   'multi_woot'      => 'woot_checker',
                   'multi_shirt'      => 'shirt_checker',
                   'multi_wine'      => 'wine_checker',
-                  'multi_sellout'      => 'sellout_checker',                  
+                  'multi_sellout'      => 'sellout_checker',
+                  'multi_kids'      => 'kids_checker',
+
+
 
 });
 
@@ -21,15 +24,18 @@ sub woot_checker {
         $uaw->timeout(15);
 
    $uaw->proxy(['http','ftp'], $self->config()->plugin_proxy()) if ($self->config()->plugin_proxy());
-   my $woot = $uaw->get('http://www.woot-tracker.net/pips/index.php');
+        my $woot = $uaw->get('http://www.woot.com/');
+        my $content = $woot->content;
 
-        if ($woot->is_success) {
-                return $self->respond( $message,($woot->content) );
 
-        } else {
-                        return $self->respond( $message,("An error has occurred.") );
-        }
+        my ($name) = $content =~ /<h2 class=\"fn\">(.+?)<\/h2/gis;
+        my ($price) = $content =~ /<span class=\"amount\">(.+?)</gis;
+        my ($url) = $content =~ /\/WantOne.aspx(.+?)\"/gis;
+
+
+        return $self->respond( $message, "$name - \$$price +\$5 Shipping - http://www.woot.com/WantOne.aspx$url\n");
 }
+
 
 sub wine_checker {
    my ($self,$message) = @_;
@@ -37,14 +43,19 @@ sub wine_checker {
         $uaw->timeout(15);
 
    $uaw->proxy(['http','ftp'], $self->config()->plugin_proxy()) if ($self->config()->plugin_proxy());
-   my $woot = $uaw->get('http://www.woot-tracker.net/pips/wine.php');
+        my $woot = $uaw->get('http://wine.woot.com/');
+        my $content = $woot->content;
 
-        if ($woot->is_success) {
-                return $self->respond( $message,($woot->content) );
 
-        } else {
-                        return $self->respond( $message,("An error has occurred.") );
-        }
+        my ($name) = $content =~ /<h2 class=\"fn\">(.+?)<\/h2/gis;
+        my ($price) = $content =~ /<span class=\"amount\">(.+?)</gis;
+        my ($url) = $content =~ /\/WantOne.aspx(.+?)\"/gis;
+
+
+        return $self->respond( $message, "$name - \$$price +\$5 Shipping - http://wine.woot.com/WantOne.aspx$url\n");
+
+
+
 }
 
 sub shirt_checker {
@@ -53,14 +64,18 @@ sub shirt_checker {
         $uaw->timeout(15);
 
    $uaw->proxy(['http','ftp'], $self->config()->plugin_proxy()) if ($self->config()->plugin_proxy());
-   my $woot = $uaw->get('http://www.woot-tracker.net/pips/shirt.php');
+        my $woot = $uaw->get('http://shirt.woot.com/');
+        my $content = $woot->content;
 
-        if ($woot->is_success) {
-                return $self->respond( $message,($woot->content) );
 
-        } else {
-                        return $self->respond( $message,("An error has occurred.") );
-        }
+        my ($name) = $content =~ /<h2 class=\"fn\">(.+?)<\/h2/gis;
+        my ($price) = $content =~ /<span class=\"amount\">(.+?)</gis;
+        my ($url) = $content =~ /\/WantOne.aspx(.+?)\"/gis;
+
+
+        return $self->respond( $message, "$name - \$$price +\$5 Shipping - http://shirt.woot.com/WantOne.aspx$url\n");
+
+
 }
 
 sub sellout_checker {
@@ -71,8 +86,8 @@ sub sellout_checker {
    $uaw->proxy(['http','ftp'], $self->config()->plugin_proxy()) if ($self->config()->plugin_proxy());
         my $woot = $uaw->get('http://shopping.yahoo.com/?name=woot');
         my $content = $woot->content;
-        
-        
+
+
         my ($name) = $content =~ /rel=\"nofollow\" >(.+?)<\/a><\/h2/gis;
         my ($price) = $content =~ /<p class=\"price\"><strong>(.+?)</gis;
         my ($url) = $content =~ /sellout.woot.com\/(.+?)\" alt/gis;
@@ -83,7 +98,30 @@ sub sellout_checker {
 
 }
 
+sub kids_checker {
+   my ($self,$message) = @_;
+   my $uaw = LWP::UserAgent->new;
+        $uaw->timeout(15);
+
+   $uaw->proxy(['http','ftp'], $self->config()->plugin_proxy()) if ($self->config()->plugin_proxy());
+        my $woot = $uaw->get('http://kids.woot.com/');
+        my $content = $woot->content;
+
+
+        my ($name) = $content =~ /<h2 class=\"fn\">(.+?)<\/h2/gis;
+        my ($price) = $content =~ /<span class=\"amount\">(.+?)</gis;
+        my ($url) = $content =~ /\/WantOne.aspx(.+?)\"/gis;
+
+
+        return $self->respond( $message, "$name - \$$price +\$5 Shipping - http://kids.woot.com/WantOne.aspx$url\n");
+
+
+}
+
+
+
 
 1;
 
 __END__
+
