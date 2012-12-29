01bf707fee7b9200a42f98c062309c9aef6f1de2
Richard Lynskey <richard@mozor.net>
2012-12-28 23:31:10 -0600

    Weather Underground powered weather module provided by jpearsal for issue #1

--- lib/PipSqueek/Plugin/Weather.pm
+++ lib/PipSqueek/Plugin/Weather.pm
@@ -1,164 +1,73 @@
 package PipSqueek::Plugin::Weather;
 use base qw(PipSqueek::Plugin);
-
-sub plugin_initialize {
-    my $self = shift;
-
-    $self->plugin_handlers(
-        {
-            'multi_w'        => 'weather',
-            'multi_weather'  => 'weather',
-            'multi_weather2' => 'weather',
-            'multi_+w'        => 'add_weather',
-            'multi_+weather'  => 'add_weather',
-            'multi_+weather2' => 'add_weather',
-
-# TODO: this should probably be handled
-#            'pipsqueek_mergeuser' => 'pipsqueek_mergeuser',
-        }
-    );
-
-    # set up our database table
-    my $schema = [
-        [ 'id',     'INTEGER PRIMARY KEY' ],
-        [ 'userid',    'INT NOT NULL' ],
-        [ 'home_loc', 'VARCHAR'],
-        [ 'last_loc', 'VARCHAR'],
-    ];
-
-    $self->dbi()->install_schema( 'weather', $schema );
-}
-
-sub add_weather {
-    my $self = shift;
-    $self->weather(@_, 1);
-}
-
-sub weather {
-    my ( $self, $message, $store_as_home_loc ) = @_;
-
-    my $weather_by_username = 0;
-    my $db_weather = $self->search_weather($message);
-
-    my $input = $message->command_input();
-       $input =~ s/\s+$//;
-       $input =~ s/^\s+//;
-
-    my $location;
-
-    # if they didn't specify any input, try to use their last_loc
-    if (!$input || $input eq "") {
-        $location = $db_weather->{'last_loc'};
-        unless ($location) {
-            $location = $db_weather->{'home_loc'};
-
-            unless ($location) {
-                return $self->respond($message, "Try !w <location>, or !+w <home location> first");
-            }
-        }
-    }
-    # otherwise, see if it's a valid username and that user has a home_loc
-    else {
-        my $user  = $self->search_user($input);
-        if ($user) {
-            $weather_by_username = 1;
-            $db_weather = $self->dbi()->select_record( 'weather',
-                { 'userid' => $user->{'id'} }
-            );
-
-            if ($db_weather) {
-                $location = $db_weather->{'home_loc'};
-            }
-        }
-        # not a valid user, let's see if they want us to store this
-        else {
-            $location = $input;
-        }
-    }
-
-    my ($response, $success) = $self->get_weather($location);
-    
-    $self->respond( $message, $response );
-
-    if ($success) {
-        unless ($weather_by_username) {
-            $db_weather->{'last_loc'} = $location;
-        }
-
-        if ($store_as_home_loc) {
-            $self->respond( $message, "Storing your home location as: $location");
-            $db_weather->{'home_loc'} = $location;
-        }
-    } else {
-        if ($store_as_home_loc) {
-            $self->respond( $message, "Location was not stored" );
-        }
-    }
-    
-    if ($db_weather) {
-        $self->dbi()->update_record( 'weather', $db_weather );
-    }
-}
-
-
-# some helper functions for the database
-sub search_weather
+ 
+use JSON;
+use URI::URL;
+use LWP::UserAgent;
+ 
+sub plugin_initialize
 {
-    my ($self,$message) = @_;
-    my $user = $self->search_or_create_user($message);
-
-    my $weather = $self->dbi()->select_record( 'weather',
-            { 'userid' => $user->{'id'} }
-            );
-
-    unless( $weather )
-    {
-        $weather =
-        $self->dbi()->create_record( 'weather',
-            { 'userid' => $user->{'id'} }
-        );
-    }
-
-    return $weather;
+  my $self = shift;
+ 
+  $self->plugin_handlers({
+      'multi_weather'    => 'weather',
+      'multi_w'          => 'weather',
+  });
 }
-
-
-sub get_weather
+ 
+sub weather
 {
-    my ($self, $input) = @_;
-
-    my $uaw = LWP::UserAgent->new;
-    $uaw->timeout(5);
-    $uaw->proxy( [ 'http', 'ftp' ], $self->config()->plugin_proxy() )
-      if ( $self->config()->plugin_proxy() );
-
-    my $gw = $uaw->get( 'http://www.google.com/ig/api?weather=' . "$input" );
-
-    unless ($gw->is_success()) {
-        return "Error reaching weather service";
-    }
-
-    my $content = $gw->content;
-
-    my ($city)       = $content =~ /city data=\"(.+?)\"\//gis;
-    my ($tempf)      = $content =~ /temp_f data=\"(.+?)\"/gis;
-    my ($tempc)      = $content =~ /temp_c data=\"(.+?)\"/gis;
-    my ($conditions) = $content =~ /condition data=\"(.+?)\"\//gis;
-    my ($humidity)   = $content =~ /humidity data=\"(.+?)\"\//gis;
-    my ($wind)       = $content =~ /wind_condition data=\"(.+?)\"\//gis;
-
-    unless ($city) {
-        return "Sorry, I can't find what you're looking for.";
-    }
-
-    return ("$city: $tempf\x{B0}F / $tempc\x{B0}C, $conditions - $humidity - $wind", 1);
+  my ($self, $message) = @_;
+  my $cmd = $message->command();
+  my $url;
+ 
+  # this could probably be better served pulled from a config. :)
+  my $wwkey = 'OH_NO_YOU_DONT_BOYO';
+ 
+  my $input = $message->command_input();
+  # quick n nastay url encoding
+  $input =~ s/ /%20/g;
+  $input =~ s/,/%2C/g;
+ 
+  $url = "http://api.wunderground.com/api/$wwkey/geolookup/conditions/q/$input.json";
+  $url = URI::URL->new($url);
+ 
+  my $browser  = LWP::UserAgent->new('agent' => 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.7.5) Gecko/20041110 Firefox/1.0');
+  my $response = $browser->get($url);
+ 
+  unless($response->is_success() && $response->content_type() eq 'application/json') {
+    return $self->respond($message, "HTTP Error or invalid content type");
+  }
+ 
+  my $results = $response->content();
+ 
+  my $decoded_json = decode_json( $results );
+ 
+  my $o_tz    = $decoded_json->{'location'}->{'tz_short'};
+  my $o_city  = $decoded_json->{'location'}->{'city'};
+  my $o_state = $decoded_json->{'location'}->{'state'};
+ 
+  my $o_t_feels_c = $decoded_json->{'current_observation'}->{'feelslike_c'};
+  my $o_t_feels_f = $decoded_json->{'current_observation'}->{'feelslike_f'};
+  my $o_t_c       = $decoded_json->{'current_observation'}->{'temp_c'};
+  my $o_t_f       = $decoded_json->{'current_observation'}->{'temp_f'};
+  my $o_t_rh      = $decoded_json->{'current_observation'}->{'relative_humidity'};
+  my $o_t_weather = $decoded_json->{'current_observation'}->{'weather'};
+  my $o_t_wind    = $decoded_json->{'current_observation'}->{'wind_string'};
+ 
+  # I thumb my nose and proper error correction
+  # Tryin' to catch me codin' dirty ..
+ 
+  if( $o_tz eq '', $o_city eq '', $o_state eq '' ) {
+    return $self->respond($message, "GENERIC ERROR MESSAGE OH F-");
+  }
+ 
+  my $output = sprintf("It is now %sF in %s, %s. The weather is %s and the wind is %s.\n", $o_t_f, $o_city, $o_state, $o_t_weather, $o_t_wind);
+ 
+  return $self->respond($message, $output);
 }
-
-
+ 
+ 
 1;
-
+ 
 __END__
-
-
-
-
