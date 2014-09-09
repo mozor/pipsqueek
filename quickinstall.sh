d97bd91e991bc410915aa9c0bc9cefc18cab5f1d
Zachary Fouts <zachary.fouts@gmail.com>
2014-09-09 12:40:08 -0500

    because the last two commits were junk.

--- quickinstall.sh
+++ quickinstall.sh
@@ -6,11 +6,11 @@ git clone https://github.com/mozor/pipsqueek.git ~/pipsqueek
 cd ~/pipsqueek/
 grep -RH ^use .|sed -e 's/.*:use //g' -e 's/;.*//g' -e 's/ .*//g'|sort|uniq > ~/pipsqueek/modules.txt
 
-if [ -f /etc/debian_version ] || [ -f /etc/ubuntu_version ]; then
+if [ -f /usr/bin/apt-get ]; then
         apt-get install --quiet curl;
         apt-get install --quiet cpan;
         curl -L http://cpanmin.us | perl - App::cpanminus
-elif [ -f /etc/centos-release ] || [ -f /etc/redhat-release ]; then
+elif [ -f /usr/bin/yum ]; then
         yum install -y --quiet cpan.
         yum groupinstall -y --quiet "Development Tools";
         curl -L http://cpanmin.us | perl - App::cpanminus
