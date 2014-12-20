ca128c86ad1dd6c8918eb3f3f83e2c1ff700ab30
Zachary Fouts <foutsz@Zachary-Fouts-MacBook-Pro.local>
2014-12-20 12:29:01 -0800

    Adding a shell init script due to fuckery from Jeff's implementation

---
+++ etc/startup.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+RUNAS=pips
+COMMAND='/usr/bin/perl /home/pips/pipsqueek/bin/pipsqueek.pl'
+ARGS=' -d /home/pips/channels/*'
+PIDFILE='/home/pips/pipsqueek.pid'
+EMAIL='ZFOUTS@FNA.bZ'
+
+function start_pipsqueek {
+	sudo -u ${RUNAS} ${COMMAND} ${ARGS} 2>/dev/null &
+	PID=$!
+	#kill -9 ${PID}
+	PIDSQUEEK=$(ps auxf | egrep ^pips.*perl|awk '{print $1}')
+	echo ${PIDSQUEEK} > ${PIDFILE}
+	
+}
+
+
+
+function stop_pipsqueek {
+	kill -9 $(cat ${PIDFILE})
+}
+
+
+function status_check {
+	if [ $(ps auxf | grep -c $(cat ${PIDFILE})) -ge 1 ]; 
+	then
+		echo RUNNING
+	else
+		echo NOT RUNNING
+	fi
+}
+
+
+function heartbeat {
+	if [ $(ps auxf | grep -c $(cat ${PIDFILE})) -ge 1 ]; 
+	then
+		exit
+	else
+		echo -e "Starting Pips\n$(uptime)\n$(dmesg)\n$(ps auxf)\n$(last)" | mail -s "Pipsqueek Restart" ${EMAIL}
+		start_pipsqueek
+	fi
+}
+
+case "$1" in
+        start)
+            start_pipsqueek
+            ;;
+
+        stop)
+            stop_pipsqueek
+            ;;
+
+        restart)
+			stop_firewall
+            start_firewall
+            ;;
+        status)
+			status_check
+            ;;			
+	    heartbeat)
+			heartbeat
+	        ;;	
+        *)
+            echo $"Usage: $0 {start|stop|restart|status}"
+            exit 1
+
+esac
